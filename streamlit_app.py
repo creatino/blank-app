cad3d2c2a43e475a6b230c216050ac494115df2f
creatino <rajsengupta2@gmail.com>
2024-12-08 14:18:50 +0000

    Version 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(" My new streamlit app")
+st.title(" My brand new streamlit app i just built")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "this is interesting"
 )
